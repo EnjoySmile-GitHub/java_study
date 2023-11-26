d99b8bef14c18ae72d4ec06ace20bb024ba95baa
EnjoySmile <enjoysmile99999@gmail.com>
2023-11-26 12:01:16 +0900

    一週間で身につくJava学習2
    ・3日目まで終了

---
+++ src/day2/Sample204.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Sample204 {
+	public static void main(String[] args) {
+		int a;
+		double b,c,d;
+		a = (int)1.23;
+		b = 1.23;
+		c = 10;
+		d = (double)c;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
